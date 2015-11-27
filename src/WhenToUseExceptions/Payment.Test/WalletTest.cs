c9b5466660aa1ae3cc0055c82c1441a666e4a380
Arialdo Martini <arialdomartini@gmail.com>
2015-11-27 23:57:05 +0100

    A wallet using exceptions to control flow

---
+++ src/WhenToUseExceptions/Payment.Test/WalletTest.cs
@@ -0,0 +1,170 @@
+﻿using NUnit.Framework;
+using FluentAssertions;
+using System;
+using NSubstitute;
+
+namespace Payment.Test
+{
+    public class WalletTest
+    {
+        Wallet sut;
+        ITaxOffice _taxOffice;
+        ILoanShark _loanShark;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _taxOffice = Substitute.For<ITaxOffice>();
+            _loanShark = Substitute.For<ILoanShark>();
+            sut = new Wallet(_taxOffice, _loanShark);
+        }
+
+        [Test]
+        public void ANewWalletIsEmpty()
+        {
+            sut.Balance.Should().Be(0);
+        }
+
+        [TestCase(100)]
+        [TestCase(0)]
+        [TestCase(1000)]
+        [TestCase(6)]
+        public void ShouldBePossibleToDepositMoney(int balance)
+        {
+            sut.Deposit(balance);
+
+            sut.Balance.Should().Be(balance);
+        }
+
+        [TestCase(0, 1, 1)]
+        [TestCase(10, 10, 20)]
+        [TestCase(5, 7, 12)]
+        [TestCase(7, 5, 12)]
+        public void DepositedMoneyAccumulate(int balance1, int balance2, int finalBalance)
+        {
+            sut.Deposit(balance1);
+            sut.Deposit(balance2);
+
+            sut.Balance.Should().Be(finalBalance);
+        }
+
+        [Test]
+        public void CannotWithdrawFromAnEmptyWallet()
+        {
+            sut.Invoking(s => s.Withdraw(2))
+
+            .ShouldThrow<EmptyWalletException>();
+        }
+
+        [TestCase(10, 10, 0)]
+        [TestCase(10, 8, 2)]
+        [TestCase(100, 0, 100)]
+        [TestCase(10, 6, 4)]
+        public void WithdrawingUpdatesTheBalance(int initialBalance, int withdrawnAmount, int finalBalance)
+        {
+            sut.Deposit(initialBalance);
+
+            sut.Withdraw(withdrawnAmount);
+
+            sut.Balance.Should().Be(finalBalance);
+        }
+
+        [TestCase(10, 100)]
+        [TestCase(10, 18)]
+        [TestCase(100, 101)]
+        [TestCase(1, 2)]
+        public void WithdrawingMoreThanAvailableDoesNotChange(int initialBalance, int withdrawnAmount)
+        {
+            sut.Deposit(initialBalance);
+
+            try {
+                sut.Withdraw(withdrawnAmount);
+            }
+            catch
+            {
+                sut.Balance.Should().Be(initialBalance);
+            }
+        }
+
+        [TestCase(10, 100)]
+        [TestCase(10, 18)]
+        [TestCase(100, 101)]
+        [TestCase(1, 2)]
+        public void WithdrawingMoreThanAvailableShouldAlertTheLoanShark(int initialBalance, int withdrawnAmount)
+        {
+            sut.Deposit(initialBalance);
+
+            try {
+                sut.Withdraw(withdrawnAmount);
+            }
+            catch
+            {
+                _loanShark.Received().Alert();
+            }
+        }
+
+        [TestCase(10, 100)]
+        [TestCase(10, 18)]
+        [TestCase(100, 101)]
+        [TestCase(1, 2)]
+        public void WithdrawingMoreThanAvailableThrowsAnException(int initialBalance, int withdrawnAmount)
+        {
+            sut.Deposit(initialBalance);
+
+            sut.Invoking(s => s.Withdraw(withdrawnAmount))
+
+           .ShouldThrow<InsufficientBalanceException>();
+        }
+
+        [TestCase(-1)]
+        [TestCase(-2)]
+        [TestCase(-100)]
+        public void CantWithdrawNegativeAmounts(int withdrawnAmount)
+        {
+            sut.Deposit(100);
+
+            sut.Invoking(s => s.Withdraw(withdrawnAmount))
+
+           .ShouldThrow<ArgumentException>();
+        }
+
+        [TestCase(1001)]
+        [TestCase(1002)]
+        [TestCase(2000)]
+        public void WithdrawingMoreThan1000EuroInvolveA2EuroTax(int requiredAmount)
+        {
+            var initialBalance = 10000;
+            sut.Deposit(initialBalance);
+
+            sut.Withdraw(requiredAmount);
+
+            sut.Balance.Should().Be(initialBalance - requiredAmount - 2);
+        }
+
+        [TestCase(1001)]
+        [TestCase(1002)]
+        [TestCase(2000)]
+        public void TaxOfficeReceivesTaxes(int requiredAmount)
+        {
+            const int initialBalance = 10000;
+            sut.Deposit(initialBalance);
+
+            sut.Withdraw(requiredAmount);
+
+            _taxOffice.Received().Pay(2);
+        }
+
+        [TestCase(1001, 1001)]
+        [TestCase(1002, 1001)]
+        [TestCase(2000, 1999)]
+        [TestCase(2000, 2000)]
+        public void ShouldThrowAnExceptionIfBalanceIsInsufficientToPayTaxes(int initialBalance, int requiredAmount)
+        {
+            sut.Deposit(initialBalance);
+
+            sut.Invoking(s => s.Withdraw(requiredAmount))
+
+           .ShouldThrow<InsufficientBalanceException>();
+        }
+    }
+}
---
+++ src/WhenToUseExceptions/Payment/Wallet.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Payment
+{   
+    public class Wallet
+    {
+        const int tax = 2;
+
+        ITaxOffice _taxOffice;
+        ILoanShark _loanShark;
+
+        public Wallet(ITaxOffice taxOffice, ILoanShark loanShark)
+        {
+            _taxOffice = taxOffice;
+            _loanShark = loanShark;
+        }
+
+        public int Balance { get; private set; }
+
+        public void Deposit(int depositedAmount)
+        {
+            Balance += depositedAmount;
+        }
+
+        public void Withdraw(int amount)
+        {
+            if (amount < 0)
+                throw new ArgumentException();
+            try
+            {
+                Balance -= TryWithdrawMoney(amount);
+            }
+            catch(NeedTaxesException)
+            {
+                _taxOffice.Pay(tax);
+                Balance -= (amount + tax);
+            }
+            catch(InsufficientBalanceException)
+            {
+                _loanShark.Alert();
+                throw;
+            }
+        }
+
+        public int TryWithdrawMoney(int wishedAmount)
+        {
+            if (Balance == 0)
+                throw new EmptyWalletException();
+            if (wishedAmount > Balance)
+            {
+                throw new InsufficientBalanceException();
+            }
+            if(wishedAmount > 1000)
+            {
+                if (Balance - wishedAmount - tax < 0)
+                    throw new InsufficientBalanceException();
+                throw new NeedTaxesException();
+            }
+            return wishedAmount;
+        }
+    }
+
+    public interface ITaxOffice
+    {
+        void Pay(int tax);
+    }
+
+    public interface ILoanShark
+    {
+        void Alert();
+    }
+
+    public class InsufficientBalanceException : Exception {}
+    public class EmptyWalletException : Exception {}
+    public class NeedTaxesException : Exception {};
+}
